18489cd311451172410e75d8b369d36389a777ee
AntonioScarnera <scarnerantonio07@gmail.com>
2022-04-01 16:10:10 +0200

    completamento

--- Esercizio 2/script.js
+++ Esercizio 2/script.js
@@ -9,31 +9,38 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-let parola = prompt("Pari o Dispari?");
-let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-
 function randomNumber (){
     let randomnumber = Math.floor((Math.random() * 5) + 1);;
     return randomnumber;
 }
-
-let pcNumber = randomNumber();
-console.log(pcNumber);
-console.log(userNumber);
-
 function somma (user, pc){
     let add = user + pc;
     return add;
 }
 
+
+
+
+let parola = prompt("Pari o Dispari?");
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+
+
+
+
+let pcNumber = randomNumber();
+console.log(pcNumber);
+console.log(userNumber);
+
+
 let numberAdd = somma(userNumber, pcNumber);
 console.log(numberAdd);
 
-if(parola !== "pari" || parola !== "Pari" || parola !== "dispari" || parola !== "Dispari"){
 
-    alert("Devi rispondere Pari o dispari");
 
-}else if((parola === "pari") || (parola === "Pari")){
+
+
+if((parola === "pari") || (parola === "Pari")){
 
     if(userNumber < 5 && userNumber > 0){
         if(numberAdd % 2 == 0){
@@ -53,4 +60,6 @@ if(parola !== "pari" || parola !== "Pari" || parola !== "dispari" || parola !== 
         }
     }
 
+}else{
+    alert("Devi rispondere pari o dispari");
 }
